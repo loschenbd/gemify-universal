cc39424c102805a699b73d79b21bc0a6e62105de
loschenbd <ben@benjaminloschen.com>
2024-06-04 10:04:44 -0700

    added new database types.

--- packages/app/utils/useGem.ts
+++ packages/app/utils/useGem.ts
@@ -1,6 +1,22 @@
 import { useQuery } from '@tanstack/react-query'
 import { useSupabase } from 'app/utils/supabase/useSupabase'
 
+export interface GemType {
+  id: number
+  title: string
+  author: string
+  audio_url: string
+  duration: number
+  main_points?: string[]
+  summary?: string
+  stories?: string[]
+  follow_up?: string[]
+  action_items?: string[]
+  bible_verses?: string[]
+  related_topics?: string[]
+  transcript?: string[]
+}
+
 function useGem(gemId?: number) {
   const supabase = useSupabase()
 
