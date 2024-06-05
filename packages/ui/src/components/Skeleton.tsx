adefbf411648ecd8a861ad16c28688e156cf83a1
loschenbd <ben@benjaminloschen.com>
2024-06-05 16:28:25 -0700

    Fixed type erros in skeleton

--- packages/ui/src/components/Skeleton.tsx
+++ packages/ui/src/components/Skeleton.tsx
@@ -5,19 +5,19 @@ export const Skeleton = () => {
     <AnimatePresence>
       <YStack miw="$20" gap="$2" p="$4">
         <XStack gap="$2" ai="center">
-          <View animation="pulse" bc="$gray4" br="$10" size="$4" o={0.5} hoverStyle={{ o: 1 }} />
+          <View animation="pulse" bc="$gray4" br="$10" h="$2" o={0.5} hoverStyle={{ o: 1 }} />
           <View
             animation="pulse"
             bc="$gray6"
             br="$4"
-            height="$2"
-            width="70%"
+            h="$2"
+            w="70%"
             o={0.5}
             hoverStyle={{ o: 1 }}
           />
         </XStack>
-        <View animation="pulse" bc="$gray6" br="$4" height="$2" o={0.5} hoverStyle={{ o: 1 }} />
-        <View animation="pulse" bc="$gray6" br="$4" height="$2" o={0.5} hoverStyle={{ o: 1 }} />
+        <View animation="pulse" bc="$gray6" br="$4" h="$2" o={0.5} hoverStyle={{ o: 1 }} />
+        <View animation="pulse" bc="$gray6" br="$4" h="$2" o={0.5} hoverStyle={{ o: 1 }} />
       </YStack>
     </AnimatePresence>
   )
