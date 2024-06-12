241a41773dec3e11333822ce9f3aecd4c901981f
loschenbd <ben@benjaminloschen.com>
2024-06-12 14:13:17 -0700

    Fixing PostHog Provider

--- packages/app/provider/posthog/PostHogProvider.native.tsx
+++ packages/app/provider/posthog/PostHogProvider.native.tsx
@@ -3,9 +3,9 @@ import { PostHogProvider as PostHogProviderNative } from 'posthog-react-native/l
 export const PostHogProvider = ({ children }: { children: React.ReactNode }) => {
   return (
     <PostHogProviderNative
-      apiKey={process.env.EXPO_PUBLIC_POSTHOG_KEY}
+      apiKey={Constants.manifest.extra.POSTHOG_KEY}
       options={{
-        host: process.env.EXPO_PUBLIC_POSTHOG_HOST,
+        host: Constants.manifest.extra.POSTHOG_HOST,,
       }}
     >
       {children}
--- packages/app/provider/posthog/PostHogProvider.tsx
+++ packages/app/provider/posthog/PostHogProvider.tsx
@@ -3,9 +3,11 @@ import { useEffect } from 'react'
 
 export const PostHogProvider = ({ children }: { children: React.ReactNode }) => {
   useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY ?? '', {
-      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    })
+    if (process.env.NEXT_PUBLIC_POSTHOG_KEY && process.env.NEXT_PUBLIC_POSTHOG_HOST) {
+      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+      })
+    }
   }, [])
 
   return <>{children}</>
